fb015feda38dda890f0211ec1ac57d36c61fa775
Ashley Winters <ashbwinters@gmail.com>
2020-03-20 01:49:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ def reduce_to_total(source_array, starting_point = 0)
   total = starting_point
   index = 0
   while index > source_array.length do
-    total += index[starting_point]
+    total += source_array[index]
     index += 1
   end
   total
